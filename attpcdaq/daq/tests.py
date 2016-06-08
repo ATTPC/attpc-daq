c2a18bc3431d36989fa1cb082eca32866f013b81
Joshua Bradt <bradt@nscl.msu.edu>
2016-06-08 17:44:49 -0400

    Started writing basic tests.

--- attpcdaq/daq/tests.py
+++ attpcdaq/daq/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import ECCServer, DataRouter, DataSource
 
-# Create your tests here.
+
+class ECCServerModelTestCase(TestCase):
+    def setUp(self):
+        self.name = 'ECC0'
+        self.ip = '127.0.0.1'
+        self.port = '8083'
+        self.ecc0 = ECCServer.objects.create(name=self.name, ip_address=self.ip, port=self.port)
+
+    def test_url(self):
+        expected = 'http://' + self.ip + ':' + self.port + '/'
+        result = self.ecc0.url
+        self.assertEqual(expected, result)
+
+
+class StatusViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        eccs = [ECCServer.objects.create(name='ECC{}'.format(i),
+                                         ip_address='192.168.41.{}'.format(i+60),
+                                         port=8083,
+                                         ) for i in range(10)]
